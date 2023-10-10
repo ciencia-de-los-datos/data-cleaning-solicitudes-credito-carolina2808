660ce984bedf3144b02a7ddd6c27e0b267ac9ee4
carolina2808 <60153934+carolina2808@users.noreply.github.com>
2023-10-10 18:24:56 -0500

    entrega 2

--- pregunta.py
+++ pregunta.py
@@ -8,13 +8,27 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 """
 import pandas as pd
 
-
 def clean_data():
-
+    # Lee el archivo CSV
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    #
-    # Inserte su código aquí
-    #
+    # Elimina filas duplicadas, si las hay
+    df.drop_duplicates(inplace=True)
+
+    # Manejo de datos faltantes (NaN)
+    
+    # df.dropna(inplace=True)
+    
+    # df.fillna(0, inplace=True)
+
+    
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], format='%d/%m/%Y', errors='coerce')
+
+    
+    df['monto_del_credito'] = pd.to_numeric(df['monto_del_credito'], errors='coerce')
+    df['estrato'] = pd.to_numeric(df['estrato'], errors='coerce')
+
+    
 
     return df
+
