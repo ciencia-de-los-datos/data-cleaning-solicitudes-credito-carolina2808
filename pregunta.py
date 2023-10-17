1ef0bdb2d91e11408953d1c3c220c3d2951069a4
carolina2808 <60153934+carolina2808@users.noreply.github.com>
2023-10-16 20:59:16 -0500

    Entrega40

--- pregunta.py
+++ pregunta.py
@@ -7,27 +7,54 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
+from datetime import datetime
 
 def clean_data():
-    # Lee el archivo CSV
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    # Elimina filas duplicadas, si las hay
+    def normalizar_fecha(fecha):
+        try:
+            # Intenta analizar la fecha en diferentes formatos
+            formatos = ['%Y/%m/%d', '%d/%m/%Y', '%y/%m/%d', '%d/%m/%y']
+            for formato in formatos:
+                try:
+                    fecha_obj = datetime.strptime(fecha, formato)
+                    return fecha_obj.strftime('%d/%m/%Y')
+                except ValueError:
+                    pass  # Formato no coincide
+            raise ValueError('Formato de fecha no reconocido')
+        except ValueError as e:
+            print(f"Error: {e}")
+            return None
+
+    # Lectura de la base de datos
+    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
+    df.reset_index(inplace=True,drop=True)
+    
+    # Checkeo de la fecha
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(normalizar_fecha)
+    
+    # Botando los duplicados y los faltantes
+    df.dropna(axis='index',inplace=True)
     df.drop_duplicates(inplace=True)
 
-    # Manejo de datos faltantes (NaN)
-    
-    df.dropna(inplace=True)
-    
-    # df.fillna(0, inplace=True)
+    # Checkeo de minusculas
+    df['sexo'] = df['sexo'].str.lower().astype(str).str.strip()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower().astype(str)
+    df['idea_negocio'] = df['idea_negocio'].str.lower().astype(str)
+    df['barrio'] = df['barrio'].str.lower().astype(str)
+    df['línea_credito'] = df['línea_credito'].str.lower().astype(str)
+     
+    # Checkeo de espacios en blanco y guiones
+    df['idea_negocio'] = df['idea_negocio'].str.replace('_',' ').str.replace('-',' ').str.strip()
+    df['barrio'] = df['barrio'].str.replace('_','-').str.replace('-',' ')
+    df['línea_credito'] = df['línea_credito'].str.replace('_',' ').str.replace('-',' ').str.strip()
 
-    
-    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], format='%d/%m/%Y', errors='coerce')
+    # Limpieza de caracteres en la variable monetaria
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',','').str.replace('$','',regex=False).str.replace(' ','').str.strip().astype(float)   
 
-    
-    df['monto_del_credito'] = pd.to_numeric(df['monto_del_credito'], errors='coerce')
-    df['estrato'] = pd.to_numeric(df['estrato'], errors='coerce')
-
-    
-
+    # Depuramos duplicados y faltantes resultantes tras la limpieza
+    df.drop_duplicates(inplace=True)
+    df.dropna(axis='index',inplace=True)
     return df
+
+
